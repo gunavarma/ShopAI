48ce36acf9e05b6911c35284e38e389061596b28
gunavarma <107696193+gunavarma@users.noreply.github.com>
2025-06-30 22:10:16 +0530

    Remove invalid patch file

--- components/orders/page.tsx
+++
@@ -1,8 +0,0 @@
-@@ .. @@
-                  <p className="text-muted-foreground mb-4">
-                    {searchQuery || statusFilter !== 'all' 
-                      ? 'Try adjusting your search or filters'
--                      : 'You haven\'t placed any orders yet'
-+                      : 'You haven&apos;t placed any orders yet'
-                     }
-                  </p>
